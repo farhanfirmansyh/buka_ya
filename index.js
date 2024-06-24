706fe61a90f169758e9f069faed1717a59625e70
farhanfirmansyh <farhanfirms@gmail.com>
2024-06-24 20:04:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const nama = prompt("Halo nama kamu siapa?");
+const p = document.getElementById('p');
+const animasi = document.getElementById("animasi");
+
+p.innerText = `Halo ${nama}, Coba tebak apa hayo?`;
+
+setTimeout(() => {
+  animasi.style.opacity = "1";
+}, 10000);
